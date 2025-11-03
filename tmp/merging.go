bb92d44e519bd533018ea0873edf284577f63766
Kyle L. Jensen <kljensen@gmail.com>
2025-11-02 22:38:47 -0500

    fix: correct API usage in merging.go
    - Fix Start()/End() method calls - changed from field access to method calls
    - Fix Get() return type handling - dereference *int pointers
    - Add nil checks for pointer comparisons
    
    Resolves compilation errors blocking test execution.

--- tmp/merging.go
+++ tmp/merging.go
@@ -5,29 +5,29 @@ import "time"
 // MergeDateTimeResult merges a date-only result with a time-only result.
 func MergeDateTimeResult(dateResult, timeResult *ParsingResult) *ParsingResult {
 	result := dateResult.Clone()
-	beginDate := dateResult.Start
-	beginTime := timeResult.Start
+	beginDate := dateResult.Start().(*ParsingComponents)
+	beginTime := timeResult.Start().(*ParsingComponents)
 
-	result.Start = MergeDateTimeComponent(beginDate, beginTime)
+	result.start = MergeDateTimeComponent(beginDate, beginTime)
 
-	if dateResult.End != nil || timeResult.End != nil {
+	if dateResult.End() != nil || timeResult.End() != nil {
 		var endDate, endTime *ParsingComponents
-		if dateResult.End == nil {
-			endDate = dateResult.Start
+		if dateResult.End() == nil {
+			endDate = dateResult.Start().(*ParsingComponents)
 		} else {
-			endDate = dateResult.End
+			endDate = dateResult.End().(*ParsingComponents)
 		}
-		if timeResult.End == nil {
-			endTime = timeResult.Start
+		if timeResult.End() == nil {
+			endTime = timeResult.Start().(*ParsingComponents)
 		} else {
-			endTime = timeResult.End
+			endTime = timeResult.End().(*ParsingComponents)
 		}
 
 		endDateTime := MergeDateTimeComponent(endDate, endTime)
 
 		// If date has no end and the merged end time is before start time,
 		// the end should be on the next day
-		if dateResult.End == nil && endDateTime.Date().Before(result.Start.Date()) {
+		if dateResult.End() == nil && endDateTime.Date().Before(result.Start().Date()) {
 			nextDay := endDateTime.Date().Add(24 * time.Hour)
 			if endDateTime.IsCertain(ComponentDay) {
 				AssignSimilarDate(endDateTime, nextDay)
@@ -36,7 +36,7 @@ func MergeDateTimeResult(dateResult, timeResult *ParsingResult) *ParsingResult {
 			}
 		}
 
-		result.End = endDateTime
+		result.end = endDateTime
 	}
 
 	return result
@@ -48,45 +48,45 @@ func MergeDateTimeComponent(dateComp, timeComp *ParsingComponents) *ParsingCompo
 
 	// Merge time components
 	if timeComp.IsCertain(ComponentHour) {
-		result.Assign(ComponentHour, timeComp.Get(ComponentHour))
-		result.Assign(ComponentMinute, timeComp.Get(ComponentMinute))
+		result.Assign(ComponentHour, *timeComp.Get(ComponentHour))
+		result.Assign(ComponentMinute, *timeComp.Get(ComponentMinute))
 
 		if timeComp.IsCertain(ComponentSecond) {
-			result.Assign(ComponentSecond, timeComp.Get(ComponentSecond))
+			result.Assign(ComponentSecond, *timeComp.Get(ComponentSecond))
 			if timeComp.IsCertain(ComponentMillisecond) {
-				result.Assign(ComponentMillisecond, timeComp.Get(ComponentMillisecond))
+				result.Assign(ComponentMillisecond, *timeComp.Get(ComponentMillisecond))
 			} else {
-				result.Imply(ComponentMillisecond, timeComp.Get(ComponentMillisecond))
+				result.Imply(ComponentMillisecond, *timeComp.Get(ComponentMillisecond))
 			}
 		} else {
-			result.Imply(ComponentSecond, timeComp.Get(ComponentSecond))
-			result.Imply(ComponentMillisecond, timeComp.Get(ComponentMillisecond))
+			result.Imply(ComponentSecond, *timeComp.Get(ComponentSecond))
+			result.Imply(ComponentMillisecond, *timeComp.Get(ComponentMillisecond))
 		}
 	} else {
-		result.Imply(ComponentHour, timeComp.Get(ComponentHour))
-		result.Imply(ComponentMinute, timeComp.Get(ComponentMinute))
-		result.Imply(ComponentSecond, timeComp.Get(ComponentSecond))
-		result.Imply(ComponentMillisecond, timeComp.Get(ComponentMillisecond))
+		result.Imply(ComponentHour, *timeComp.Get(ComponentHour))
+		result.Imply(ComponentMinute, *timeComp.Get(ComponentMinute))
+		result.Imply(ComponentSecond, *timeComp.Get(ComponentSecond))
+		result.Imply(ComponentMillisecond, *timeComp.Get(ComponentMillisecond))
 	}
 
 	// Merge timezone
 	if timeComp.IsCertain(ComponentTimezoneOffset) {
-		result.Assign(ComponentTimezoneOffset, timeComp.Get(ComponentTimezoneOffset))
+		result.Assign(ComponentTimezoneOffset, *timeComp.Get(ComponentTimezoneOffset))
 	}
 
 	// Merge meridiem
 	if timeComp.IsCertain(ComponentMeridiem) {
-		result.Assign(ComponentMeridiem, timeComp.Get(ComponentMeridiem))
-	} else if timeComp.Get(ComponentMeridiem) != 0 && result.Get(ComponentMeridiem) == 0 {
-		result.Imply(ComponentMeridiem, timeComp.Get(ComponentMeridiem))
+		result.Assign(ComponentMeridiem, *timeComp.Get(ComponentMeridiem))
+	} else if timeComp.Get(ComponentMeridiem) != nil && *timeComp.Get(ComponentMeridiem) != 0 && result.Get(ComponentMeridiem) != nil && *result.Get(ComponentMeridiem) == 0 {
+		result.Imply(ComponentMeridiem, *timeComp.Get(ComponentMeridiem))
 	}
 
 	// Apply PM meridiem adjustment
-	if result.Get(ComponentMeridiem) == MeridiemPM && result.Get(ComponentHour) < 12 {
+	if result.Get(ComponentMeridiem) != nil && *result.Get(ComponentMeridiem) == int(MeridiemPM) && result.Get(ComponentHour) != nil && *result.Get(ComponentHour) < 12 {
 		if timeComp.IsCertain(ComponentHour) {
-			result.Assign(ComponentHour, result.Get(ComponentHour)+12)
+			result.Assign(ComponentHour, *result.Get(ComponentHour)+12)
 		} else {
-			result.Imply(ComponentHour, result.Get(ComponentHour)+12)
+			result.Imply(ComponentHour, *result.Get(ComponentHour)+12)
 		}
 	}
 
